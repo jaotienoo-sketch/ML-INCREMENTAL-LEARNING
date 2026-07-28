7f208662fc7547dabb9b58b40f2df7e85f7908eb
daddikim <jimmy.wanyama@gmail.com>
2026-07-28 18:08:28 +0300

    Update __init__.py

--- pycode/__init__.py
+++ pycode/__init__.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import SGDClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.utils import shuffle
+
+df = pd.read_csv("cropyields.csv")
